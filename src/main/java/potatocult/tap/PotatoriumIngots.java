bb24b069dea6e28a450ef3faf1f8e7c3a2e90b37
crispy_chips1234 <2222080225@qq.com>
2020-04-23 18:21:15 +0800

    Sorta added a half working ItemGroup

--- src/main/java/potatocult/tap/PotatoriumIngots.java
+++ src/main/java/potatocult/tap/PotatoriumIngots.java
@@ -13,10 +13,11 @@ import snownee.kiwi.item.ModItem;
 
 @KiwiModule
 public class PotatoriumIngots extends AbstractModule {
-    public static final Item REFINED_POTATORIUM = new ModItem(new Item.Properties().rarity(Rarity.RARE));
+    public static final Item REFINED_POTATORIUM = new ModItem(new Item.Properties().rarity(Rarity.RARE).group(TapItemGroup.TAP_ITEMS));
 
-    public static final Item POTATORIUM = new ModItem(new Item.Properties());
+    public static final Item POTATORIUM = new ModItem(new Item.Properties().group(TapItemGroup.TAP_ITEMS));
 
     //super stupid long line warning
+    @KiwiModule.Group("TAP_ITEMS")
     public static final Block REFINED_POTATORIUM_BLOCK = new ModBlock(Block.Properties.create(Material.IRON).harvestLevel(2).harvestTool(ToolType.PICKAXE).hardnessAndResistance(5.0F, 6.0F).sound(SoundType.CROP));
 }
